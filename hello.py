59a8bc46920d17aaa88e97466c0db1ae3681b1f7
Oyinda David <oyindadavid18@gmail.com>
2020-09-25 14:27:15 +0100

    added the time module

--- hello.py
+++ hello.py
@@ -1,12 +1,13 @@
-def bot_response():
-    name = input('Hello, this is OG bot, what is your name? \n')
-    welcome_message = 'Welcome {name}, have a great time'
-    return name
+import time
 
-def user_response():
-    your_name = bot_response()
-    response = 'My name is {0}'.format(your_name)
-    return response
+def respond(message):
+    time.sleep(0.5)
+    return "I can hear you, you said: {0}".format(message)
 
-user_response()
 
+def send_message():
+    message = input("")
+    return respond(message)
+
+
+print(send_message())
